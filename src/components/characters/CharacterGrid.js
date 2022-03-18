695a609cb43cb27574ba457958184d7773a729c0
Niraj Karkiclear <noisyking7@gmail.com>
2022-03-18 19:33:45 +0545

    all done

---
+++ src/components/characters/CharacterGrid.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import CharacterItem from './Characteritem'
+
+
+
+const CharacterGrid = ({items,isLoading}) => {
+  return isLoading ? (
+    <h1>Loading...</h1>
+     ) : (
+  <section className="cards">
+      {items.map(item => (
+         <CharacterItem key={item.char_id} item={item}></CharacterItem> 
+      ))}
+  </section>
+  )
+} 
+
+export default CharacterGrid
